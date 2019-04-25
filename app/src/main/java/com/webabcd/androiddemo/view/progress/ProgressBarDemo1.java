7b898659d957e342a68c2008833fd7c2eedb3609
webabcd <webabcd@hotmail.com>
2019-04-25 17:06:08 +0800

    update

---
+++ app/src/main/java/com/webabcd/androiddemo/view/progress/ProgressBarDemo1.java
@@ -0,0 +1,19 @@
+/**
+ * ProgressBar - 进度条控件
+ */
+
+package com.webabcd.androiddemo.view.progress;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.webabcd.androiddemo.R;
+
+public class ProgressBarDemo1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_progress_progressbardemo1);
+    }
+}
