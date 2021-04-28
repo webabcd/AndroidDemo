7984754b3571c03bc22392a320f87c4ce7b9f258
webabcd <webabcd@hotmail.com>
2021-04-28 17:08:41 +0800

    update

--- app/src/main/java/com/webabcd/androiddemo/activity/ActivityDemo6.java
+++ app/src/main/java/com/webabcd/androiddemo/activity/ActivityDemo6.java
@@ -1,7 +1,7 @@
 /**
- * Activity 样式（隐藏状态栏，对话框样式）
+ * Activity 样式（隐藏状态栏，改变状态栏颜色，对话框样式的 activity）
  *
- * 本例演示如何隐藏 activity 的 statusBar
+ * 本例演示如何隐藏 activity 的 statusBar，以及如何改变 statusBar 的颜色
  * 关于对话框样式的 activity 请参见 activity/ActivityDemo6_2
  */
 
@@ -21,6 +21,8 @@ public class ActivityDemo6 extends AppCompatActivity {
     private Button mButton1;
     private Button mButton2;
     private Button mButton3;
+    private Button mButton4;
+    private Button mButton5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +32,8 @@ public class ActivityDemo6 extends AppCompatActivity {
         mButton1 = findViewById(R.id.button1);
         mButton2 = findViewById(R.id.button2);
         mButton3 = findViewById(R.id.button3);
+        mButton4 = findViewById(R.id.button4);
+        mButton5 = findViewById(R.id.button5);
 
         sample();
     }
@@ -57,6 +61,23 @@ public class ActivityDemo6 extends AppCompatActivity {
             public void onClick(View v) {
                 // 显示 statusBar
                 getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            }
+        });
+
+        mButton4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 改变 statusBar 的背景色
+                getWindow().setStatusBarColor(getResources().getColor(R.color.green));
+            }
+        });
+
+        mButton5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 改变 statusBar 的前景色
+                // 这个只有 2 种颜色，要么黑要么白
+                getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
             }
         });
     }
