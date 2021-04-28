4e5a7f96e50fb67d9168e1f3eb0e6eeca5634905
webabcd <webabcd@hotmail.com>
2021-04-28 15:14:51 +0800

    update

--- app/src/main/java/com/webabcd/androiddemo/view/ViewDemo3.java
+++ app/src/main/java/com/webabcd/androiddemo/view/ViewDemo3.java
@@ -1,21 +1,38 @@
+/**
+ * 演示布局文件中的 include 的用法
+ */
+
 package com.webabcd.androiddemo.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.webabcd.androiddemo.R;
 
 public class ViewDemo3 extends AppCompatActivity {
+
+    private TextView mTextView1;
+    private TextView mTextView2;
+    private TextView mTextView3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_viewdemo3);
 
+        mTextView1 = findViewById(R.id.textView1);
+        mTextView2 = findViewById(R.id.textView2);
+        mTextView3 = findViewById(R.id.textView3);
+
         sample();
     }
 
     private void sample() {
-
+        mTextView1.setBackgroundColor(Color.RED);
+        mTextView2.setBackgroundColor(Color.GREEN);
+        mTextView3.setBackgroundColor(Color.BLUE);
     }
 }
