e41638bf4901c00226fbf92cb6cce4d569f91bee
webabcd <webabcd@hotmail.com>
2023-03-21 12:46:56 +0800

    update

--- app/src/main/java/com/webabcd/androiddemo/resource/MetaDataDemo1.java
+++ app/src/main/java/com/webabcd/androiddemo/resource/MetaDataDemo1.java
@@ -2,9 +2,10 @@
  * 读取 meta-data 数据
  *
  * 在 AndroidManifest.xml 的 application 节点或 activity 节点或 service 节点或 receiver 节点下可以配置 meta-data 数据
- * 本例以在 application 节点下配置如下数据为例
+ * 本例以在 application 节点下配置如下数据为例，你可以获取 value 指定的值或 resource 指定的资源标识
  * <meta-data android:name="com.webabcd.androiddemo.MetaData1" android:value="abc" />
  * <meta-data android:name="com.webabcd.androiddemo.MetaData2" android:value="@string/sample_hello1" />
+ * <meta-data android:name="com.webabcd.androiddemo.MetaData3" android:resource="@string/sample_hello1" />
  */
 
 package com.webabcd.androiddemo.resource;
@@ -45,8 +46,17 @@ public class MetaDataDemo1 extends AppCompatActivity {
              * 通过 getPackageManager().getReceiverInfo() 可以获取 receiver 节点下的 meta-data 数据
              */
             ApplicationInfo info = this.getPackageManager().getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA);
+
+            // 获取 <meta-data android:name="com.webabcd.androiddemo.MetaData1" android:value="abc" /> 中指定的值
             result = "MetaData1:" + info.metaData.getString("com.webabcd.androiddemo.MetaData1");
+
+            // 获取 <meta-data android:name="com.webabcd.androiddemo.MetaData2" android:value="@string/sample_hello1" /> 中指定的值
             result += ", MetaData2:" + info.metaData.getString("com.webabcd.androiddemo.MetaData2");
+
+            // 获取 <meta-data android:name="com.webabcd.androiddemo.MetaData3" android:resource="@string/sample_hello1" /> 中指定的资源 id
+            int resourceId = info.metaData.getInt("com.webabcd.androiddemo.MetaData3");
+            result += ", MetaData3:" + getResources().getString(resourceId);
+
         } catch (PackageManager.NameNotFoundException e) {
             result = e.toString();
         }
